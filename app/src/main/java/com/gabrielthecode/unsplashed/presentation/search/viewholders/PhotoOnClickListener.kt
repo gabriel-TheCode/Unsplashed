2e5a635f18f87ae7ce524a14b71bb93feefe7f94
Gabriel TEKOMBO <tekombo.gabriel@gmail.com>
2024-10-09 13:48:07 +0200

    clean up code

--- app/src/main/java/com/gabrielthecode/unsplashed/presentation/search/viewholders/PhotoOnClickListener.kt
+++
@@ -1,8 +0,0 @@
-package com.gabrielthecode.unsplashed.presentation.search.viewholders
-
-import android.view.View
-
-interface PhotoOnClickListener {
-	fun openPhoto(photo: PhotoUiModel)
-	fun showImageFullResolution(photo: PhotoUiModel, view: View)
-}
